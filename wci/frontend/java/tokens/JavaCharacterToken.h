cb668957a8d1f822a5040adfeca724b51242498a
Alex Norell <alex@alexnorell.com>
2017-09-11 16:15:35 -0700

    Added Character Token boilerplate files

---
+++ wci/frontend/java/tokens/JavaCharacterToken.h
@@ -0,0 +1,42 @@
+/**
+ * <h1>PascalStringToken</h1>
+ *
+ * <p> Pascal string tokens.</p>
+ *
+ * <p>Copyright (c) 2017 by Ronald Mak</p>
+ * <p>For instructional purposes only.  No warranties.</p>
+ */
+#ifndef WCI_FRONTEND_PASCAL_TOKENS_PASCALSTRINGTOKEN_H_
+#define WCI_FRONTEND_PASCAL_TOKENS_PASCALSTRINGTOKEN_H_
+
+#include <string>
+
+#include "../JavaToken.h"
+
+namespace wci { namespace frontend { namespace java { namespace tokens {
+
+                using namespace std;
+                using namespace wci::frontend::java;
+
+                class PascalStringToken : public PascalToken
+                {
+                public:
+                    /**
+                     * Constructor.
+                     * @param source the source from where to fetch the token's characters.
+                     * @throw a string message if an error occurred.
+                     */
+                    PascalStringToken(Source *source) throw (string);
+
+                protected:
+                    /**
+                     * Extract a Pascal string token from the source.
+                     * Override of wci::frontend::Token.
+                     * @throw a string message if an error occurred.
+                     */
+                    void extract() throw (string);
+                };
+
+            }}}}  // namespace wci::frontend::pascal::tokens
+
+#endif /* WCI_FRONTEND_PASCAL_TOKENS_PASCALSTRINGTOKEN_H_ */
