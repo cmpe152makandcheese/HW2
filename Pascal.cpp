c7c44f9e92fa3517ade0df858b3543f58e7274a6
Alex Norell <alex@alexnorell.com>
2017-09-17 16:40:33 -0700

    removed broken replace function

--- Pascal.cpp
+++ Pascal.cpp
@@ -44,16 +44,6 @@ const string USAGE =
  */
 
 
-//void replace(std::string& str, const std::string& from, const std::string& to) {
-//    if(from.empty())
-//        return;
-//    size_t start_pos = 0;
-//    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-//        str.replace(start_pos, from.length(), to);
-//        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
-//    }
-//}
-
 void replace_in_place(std::string& subject, const std::string& search,
                           const std::string& replace) {
     size_t pos = 0;
