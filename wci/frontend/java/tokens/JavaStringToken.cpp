da94b27d02fa8143c1de68e8bbffeb4db98e9ae4
Alexis <alexischeungho@gmail.com>
2017-09-17 15:16:43 -0700

    Add corner case for string

--- wci/frontend/java/tokens/JavaStringToken.cpp
+++ wci/frontend/java/tokens/JavaStringToken.cpp
@@ -36,6 +36,18 @@ void JavaStringToken::extract() throw (string)
         // Replace any whitespace character with a blank.
         if (isspace(current_ch)) current_ch = ' ';
 
+        // Not the end of string
+		if (current_ch == '\\' && peek_char() == '\"')
+		{
+			text += current_ch;
+			value_str  += current_ch;
+			current_ch = next_char();  // consume character \
+
+			text += current_ch;
+			value_str  += current_ch;
+			current_ch = next_char();  // consume character "
+		}
+
         if ((current_ch != '\"') && (current_ch != EOF))
         {
             text += current_ch;
@@ -55,6 +67,7 @@ void JavaStringToken::extract() throw (string)
             }
         }
     } while ((current_ch != '\"') && (current_ch != Source::END_OF_FILE));
+
 
     if (current_ch == '\"')
     {
