f2a9d28a350ef557ba314dbed124e48d274495d9
Alex Norell <alex@alexnorell.com>
2017-09-17 15:04:44 -0700

    Fixed issue with CHAR 'value' attribute printing out wrong values

--- wci/frontend/java/JavaParserTD.cpp
+++ wci/frontend/java/JavaParserTD.cpp
@@ -62,7 +62,7 @@ void JavaParserTD::parse() throw (string)
             case PT_CHAR:
             {
                 type_str = "CHAR";
-                value_str = token->get_value()->c;
+                value_str = token->get_value()->s;
                 break;
             }
 
